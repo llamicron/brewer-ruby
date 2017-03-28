8951674601194a73ee61d877667dc71a773b4adb
Luke Sweeney <luke@thesweeneys.org>
2017-03-28 10:38:15 -0500

    adds more tests for adaptibrew with rspec i figured it out!

--- spec/adaptibrew_spec.rb
+++ spec/adaptibrew_spec.rb
@@ -3,14 +3,60 @@ require_relative 'spec_helper'
 describe Adaptibrew do
 
   before :each do
-    @adaptibrew = Adaptibrew.new
+    @adaptibrew = Adaptibrew.new.refresh
   end
 
   describe "#new" do
-    it "returns a new adaptibrew object" do
-      expect(@adaptibrew).to be_an_instance_of Adaptibrew
-    end
+    let(:adaptibrew) { Adaptibrew.new }
+    specify { expect(adaptibrew).to be_an_instance_of Adaptibrew }
   end
 
+  describe ".clear" do
+    context "when the repo already exists" do
+      specify { expect(Dir.exists?('adaptibrew')).to be true }
+
+      it "deletes the repo" do
+        @adaptibrew.clear
+        expect(Dir.exists?('adaptibrew')).to be false
+      end
+    end
+
+    context "when the repo does not exist" do
+      let(:adaptibrew) { Adaptibrew.new }
+      before { adaptibrew.clear }
+      specify { expect(Dir.exists?('adaptibrew')).to be false }
+
+      it "does nothing" do
+        adaptibrew.clear
+        expect(Dir.exists?('adaptibrew')).to be false
+      end
+    end
+
+  end
+
+  describe ".clone" do
+    context "when the repo does not exist" do
+      let(:adaptibrew) { Adaptibrew.new }
+      before { adaptibrew.clear }
+      specify { expect(Dir.exists?('adaptibrew')).to be false }
+
+      it "clones the repo" do
+        adaptibrew.clone
+        expect(Dir.exists?('adaptibrew')).to be true
+      end
+    end
+
+    context "when the repo exists" do
+      let(:adaptibrew) { Adaptibrew.new }
+      before { adaptibrew.clone }
+      specify { expect(Dir.exists?('adaptibrew')).to be true }
+
+      it "does nothing" do
+        adaptibrew.clone
+        expect(Dir.exists?('adaptibrew')).to be true
+      end
+    end
+
+  end
 
 end
