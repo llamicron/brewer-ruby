4e32cddb621f4204b2494843794ebf2821888e21
Luke Sweeney <luke@thesweeneys.org>
2017-03-30 16:41:32 -0500

    adds comment to .slack.yml

--- lib/slacker.rb
+++ lib/slacker.rb
@@ -2,8 +2,8 @@ require 'slack-notifier'
 require 'yaml'
 require 'yaml/store'
 
-if !File.file?('slack.yml')
-  store = YAML::Store.new 'slack.yml'
+if !File.file?('.slack.yml')
+  store = YAML::Store.new '.slack.yml'
 
   print "Enter your Slack webhook url: "
   webhook_url = gets.chomp
@@ -13,4 +13,9 @@ if !File.file?('slack.yml')
   end
 end
 
-$slack = Slack::Notifier.new YAML.load(File.open('slack.yml'))['webhook_url']
+File.open(".slack.yml", 'a') do |file|
+  file.puts "# This is the slack configuration file for the brewer gem"
+  file.puts "# You can delete this file and brewer will re-create it on start-up"
+end
+
+$slack = Slack::Notifier.new YAML.load(File.open('.slack.yml'))['webhook_url']
