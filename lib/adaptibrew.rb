9eac9a2cd8bcfb0e2c403440b3a0828e833004a9
Luke Sweeney <luke@thesweeneys.org>
2017-03-31 13:37:49 -0500

    hopefully a fix in adaptibrew.rb. i hate paths.

--- lib/adaptibrew.rb
+++ lib/adaptibrew.rb
@@ -8,7 +8,7 @@ include Helpers
 class Adaptibrew
 
   def initialize
-    @install_dir = Dir.home + '/.brewer/'
+    @install_dir = Dir.home + '/.brewer/adaptibrew/'
   end
 
   # If used in IRB, Ripl, etc. it will clone into the directory IRB was started in
@@ -30,7 +30,7 @@ class Adaptibrew
       end
     end
     # :nocov:
-    FileUtils.rm_rf(@install_dir + "adaptibrew")
+    FileUtils.rm_rf(@install_dir)
     self
   end
 
@@ -42,7 +42,7 @@ class Adaptibrew
   end
 
   def present?
-    if Dir.exists?(@install_dir + "adaptibrew")
+    if Dir.exists?(@install_dir)
       return true
     end
     false
